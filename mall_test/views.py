80ede50bef684760dd9be47aac9c0cc3a08b1657
zion <0o0w0d@gmail.com>
2024-12-22 19:19:43 +0900

    add payment_new view form processing

--- mall_test/views.py
+++ mall_test/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from mall_test.models import Payment
+from mall_test.forms import PaymentForm
 
 
 # Create your views here.
 def payment_new(request):
-    return render(request, "mall_test/payment_form.html")
+    if request.method == "POST":
+        form = PaymentForm(request.POST)
+        if form.is_valid():
+            payment = form.save()
+            return redirect("payment_pay", pk=payment.pk)
+    else:
+        form = PaymentForm()
+    return render(request, "mall_test/payment_form.html", {"form": form})
+
+
+def payment_pay(request, pk):
+    payment = get_object_or_404(Payment, pk=pk)
+    return render(request, "mall_test/payment_pay.html", {"payment": payment})
