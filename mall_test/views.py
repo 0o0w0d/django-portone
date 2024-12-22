38cc565125b904ee0f4126ba5d53ca2a523edd51
zion <0o0w0d@gmail.com>
2024-12-22 19:36:00 +0900

    add payment_pay view

--- mall_test/views.py
+++ mall_test/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
 from mall_test.models import Payment
 from mall_test.forms import PaymentForm
@@ -18,4 +19,18 @@ def payment_new(request):
 
 def payment_pay(request, pk):
     payment = get_object_or_404(Payment, pk=pk)
-    return render(request, "mall_test/payment_pay.html", {"payment": payment})
+    payment_props = {
+        "merchant_uid": payment.merchant_uid,
+        "name": payment.name,
+        "amount": payment.amount,
+    }
+    payment_check_url = reverse("payment_check", args=[payment.pk])
+    return render(
+        request,
+        "mall_test/payment_pay.html",
+        {"payment_props": payment_props, "payment_check_url": payment_check_url},
+    )
+
+
+def payment_check(request, pk):
+    pass
