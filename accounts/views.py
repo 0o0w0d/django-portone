f1be7e02fe86b8bb8d057adf785d93d2e18a0251
zion <0o0w0d@gmail.com>
2024-12-25 17:33:23 +0900

    (accounts) add signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,28 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from accounts.forms import SignupForm
+from accounts.models import User
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = "accounts/signup_form.html"
+    success_url = reverse_lazy("login")
+
+
+signup = SignupView.as_view()
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def profile(request):
+    pass
