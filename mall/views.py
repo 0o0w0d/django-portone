2291c80c12b79eb9575bb95e931cae6578bc5a40
zion <0o0w0d@gmail.com>
2024-12-28 21:30:51 +0900

    (mall) add product_list page

--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from mall.models import Product
+
+
 # Create your views here.
+def product_list(request):
+    qs = Product.objects.select_related("category")
+    return render(request, "mall/product_list.html", {"product_list": qs})
