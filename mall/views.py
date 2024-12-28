f2d77508f51c064f5f86836c57214a575c43a5a7
zion <0o0w0d@gmail.com>
2024-12-29 00:14:34 +0900

    (mall) add pagination to product list page

--- mall/views.py
+++ mall/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from mall.models import Product
 
@@ -7,3 +8,12 @@ from mall.models import Product
 def product_list(request):
     qs = Product.objects.select_related("category")
     return render(request, "mall/product_list.html", {"product_list": qs})
+
+
+class ProductListView(ListView):
+    model = Product
+    queryset = Product.objects.select_related("category")
+    paginate_by = 12
+
+
+product_list = ProductListView.as_view()
